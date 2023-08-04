59d97a9607a12c4f3e886cee758623b194440522
kAleks12 <kacper4553@gmail.com>
2023-08-04 11:44:31 +0200

    Improve resource management + code refactor and cleanup -add limit for network runs saved in filesystem -add removing previous runs on restart

--- src/service/image_classification_service.py
+++ src/service/image_classification_service.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import re
+import shutil
 
 from ultralytics import YOLO
 
@@ -10,9 +11,10 @@ from src.utils.config_parser import parser
 logger = logging.getLogger('ImageClassificationService')
 
 # paths for yolo
-__yolo_old_run_path = parser.get_attr('yolo', 'old_run_path')
-__yolo_labels_path = parser.get_attr('yolo', 'labels_path')
+__old_run_path = parser.get_attr('yolo', 'old_run_path')
+__labels_path = parser.get_attr('yolo', 'labels_path')
 __input_img_data_path = parser.get_attr('yolo', 'img_data_path')
+__image_threshold = int(parser.get_attr('yolo', 'image_threshold'))
 
 # creating input dir if not initialized
 if not os.path.isdir(__input_img_data_path):
@@ -20,7 +22,7 @@ if not os.path.isdir(__input_img_data_path):
 
 # removing old run dir if exists
 try:
-    os.remove(__yolo_old_run_path)
+    shutil.rmtree(__old_run_path)
 except WindowsError as e:
     logger.error(e)
 __model = YOLO(parser.get_attr('yolo', 'model_weights'))
@@ -33,14 +35,13 @@ def classify(image_file: any) -> TagsDTO:
         file.write(content)
 
     __model(full_path, save_txt=True, imgsz=640)
-    os.remove(full_path)
 
     return TagsDTO(tags=_parse_labels(image_file.filename))
 
 
 def _parse_labels(image_name: str) -> list[str]:
-    image_name = re.sub(rf'\..*', '.txt', image_name)
-    full_path = "".join(__yolo_labels_path + image_name)
+    image_output_name = re.sub(rf'\..*', '.txt', image_name)
+    full_path = "".join(__labels_path + image_output_name)
 
     with open(full_path) as file:
         mapped_labels = {}
@@ -50,4 +51,18 @@ def _parse_labels(image_name: str) -> list[str]:
 
         results = [mapped_label[0] for mapped_label in mapped_labels.items() if mapped_label[1] > 0.0]
 
+    _cleanup(__input_img_data_path + image_name)
+
     return results
+
+
+def _cleanup(input_file_path: str):
+    os.remove(input_file_path)
+
+    file_counter = 0
+    for obj in os.listdir(__labels_path):
+        if os.path.isfile(__labels_path + obj):
+            file_counter += 1
+
+    if file_counter > __image_threshold:
+        shutil.rmtree(__old_run_path)
